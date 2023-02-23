1ec77d3bd4abfe555c9b4ef0f27da65fbc639bb5
Fernanda Winter <fernanda.winter@hotmail.com>
2023-02-23 17:12:15 -0300

    adding information about react - JSX - hello world

--- 1 - JSX/2 - hello-world.js
+++ 1 - JSX/2 - hello-world.js
@@ -21,3 +21,17 @@
 // const h1 = <h1>Hello world</h1>;
 // Click Run when you’re finished.
 
+
+// The Mystery, Revealed
+// Good!
+
+// Take another look at the line of code that you wrote.
+
+// Does this code belong in a JavaScript file, an HTML file, or somewhere else?
+
+// The answer is…a JavaScript file! Despite what it looks like, your code doesn’t actually contain any HTML at all.
+
+// The part that looks like HTML, <h1>Hello world</h1>, is something called JSX.
+
+// Click Next to learn about JSX.
+
