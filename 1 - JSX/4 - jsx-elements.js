0ed6cefb31382311450cf6a6a203e70204cbb429
Fernanda Winter <fernanda.winter@hotmail.com>
2023-02-25 11:23:01 -0300

    adding information about react - JSX - jsx elements

---
+++ 1 - JSX/4 - jsx-elements.js
@@ -0,0 +1,14 @@
+// JSX Elements
+// A basic unit of JSX is called a JSX element.
+
+//     Here’s an example of a JSX element:
+
+// <h1>Hello world</h1>
+// This JSX element looks exactly like HTML! The only noticeable difference is that you would find it in a JavaScript file, instead of in an HTML file.
+
+
+
+
+// In app.js, write a JSX <p></p> element containing the text, Hello world. Use the example code above as a guide.
+
+<p>Hello World</p>
