82d352c9c449456bfe45499c436626816647f0c7
Fernanda Winter <fernanda.winter@hotmail.com>
2023-03-15 16:46:46 -0300

    adding information about react - JSX - nested JSX

--- 1 - JSX/7 - nested-jsx.js
+++ 1 - JSX/7 - nested-jsx.js
@@ -29,3 +29,12 @@
 //      </h1>
 //    </a>
 //  );
+
+// Declare a new variable named myDiv. Set myDiv equal to a JSX <div></div> element.
+// Wrap the <div></div> in parentheses, and use indentation and line breaks like in the examples. In between the <div></div> tags, nest an <h1></h1> containing the text Hello world.
+
+const myDiv = (
+  <div>
+    <h1>Hello world</h1>
+  </div>
+);
