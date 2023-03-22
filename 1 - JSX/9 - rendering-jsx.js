ae5e8c86350688c1926688a64ae3c233a5935fc1
Fernanda Winter <fernanda.winter@hotmail.com>
2023-03-21 23:12:43 -0300

    adding information about react - JSX - rendering jsx

---
+++ 1 - JSX/9 - rendering-jsx.js
@@ -0,0 +1,18 @@
+// Rendering JSX
+// You’ve learned how to write JSX elements! Now it’s time to learn how to render them.
+
+// To render a JSX expression means to make it appear onscreen.
+
+
+
+
+// The following code will render a JSX expression:
+// ReactDOM.render(<h1>Hello world</h1>, document.getElementById('app'));
+// Starting on line 5, carefully copy the code into the code editor. We’ll go over how it works in the next exercise.
+// JavaScript is case-sensitive, so make sure to capitalize ReactDOM correctly!
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Copy code here:
+ReactDOM.render(<h1>Hello world</h1>, document.getElementById('app'));
