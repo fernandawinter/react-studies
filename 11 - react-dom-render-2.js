99f200d5ecf400604d65123ec24c3f29ff706a19
Fernanda Winter <fernanda.winter@hotmail.com>
2023-03-28 20:19:35 -0300

    adding information about react - JSX - react dom render - 2

--- 11 - react-dom-render-2.js
+++ 11 - react-dom-render-2.js
@@ -25,3 +25,30 @@
 // Select app.js.
 // You want <h1>Render me!</h1> to be appended to <span id="container"></span>.
 // On line 5, make that happen by changing the string passed to document.getElementById().
+
+app.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+ReactDOM.render(<h1>Render me!</h1>, document.getElementById('container'));
+
+
+index.html
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="/styles.css">
+	<title>Learn ReactJS</title>
+</head>
+
+<body>
+  <span id="container"></span>
+	<script src="https://content.codecademy.com/courses/React/react-course-bundle.min.js"></script>
+  <script src="/app.compiled.js"></script>
+</body>
+
+</html>
