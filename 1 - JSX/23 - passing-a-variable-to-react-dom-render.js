70b5bec3c04e313e3280df0603453b60676d67e3
Fernanda Winter <fernanda.winter@hotmail.com>
2023-03-30 17:32:24 -0300

    adding information about react - JSX - passing a variable to react dom render

--- 1 - JSX/23 - passing-a-variable-to-react-dom-render.js
+++ 1 - JSX/23 - passing-a-variable-to-react-dom-render.js
@@ -11,8 +11,29 @@
 //     <li>Become a Developer</li>
 //   </ol>
 // );
- 
+
 // ReactDOM.render(
-//   toDoList, 
+//   toDoList,
 //   document.getElementById('app')
 // );
+
+// On line 5, declare a variable named myList. Set myList equal to a JSX <ul></ul> element. Wrap your <ul></ul> in parentheses.
+// Add several <li></li> elements in between your <ul></ul> tags. Put some text in each <li></li>. Use line breaks and indentation similar to the above example.
+
+// At the bottom of the file, call ReactDOM.render().
+// For ReactDOM.render()‘s first argument, pass in the variable myList.
+// For ReactDOM.render()‘s second argument, select an HTML element with an id of app.
+// Feel free to use the example code as a guide.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Write code here:
+const myList = (
+  <ul>
+    <li>Learn React</li>
+    <li>Become a Developer</li>
+  </ul>
+);
+
+ReactDOM.render(myList, document.getElementById("app"));
