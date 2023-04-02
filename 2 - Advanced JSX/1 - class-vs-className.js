68447f293692591fb9bced1159ed725e98da393b
Fernanda Winter <fernanda.winter@hotmail.com>
2023-04-02 20:02:45 -0300

    adding information about react - Advanced JSX - class vs className

--- 2 - Advanced JSX/1 - class-vs-className.js
+++ 2 - Advanced JSX/1 - class-vs-className.js
@@ -12,3 +12,21 @@
 // This is because JSX gets translated into JavaScript, and class is a reserved word in JavaScript.
 
 // When JSX is rendered, JSX className attributes are automatically rendered as class attributes.
+
+// On line 5, declare a new variable named myDiv. Set myDiv equal to a JSX <div></div> element.
+// In between the <div></div> tags, write the text I AM A BIG DIV.
+// Give your <div></div> the following attribute:
+// className="big"
+
+// Underneath your <div></div>, call ReactDOM.render.
+// For ReactDOM.render()‘s first argument, pass in myDiv.
+// For ReactDOM.render()‘s second argument, pass in document.getElementById('app').
+// If your rendered <div></div> has a class of "big", then it should look big in the browser!
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Write code here:
+const myDiv = <div className="big">I AM A BIG DIV</div>;
+
+ReactDOM.render(myDiv, document.getElementById("app"));
