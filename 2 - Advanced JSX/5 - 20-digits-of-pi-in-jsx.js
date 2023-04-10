379c6dc5fa55b44f17a63ffe0596fb73d8259b71
Fernanda Winter <fernanda.winter@hotmail.com>
2023-04-10 17:37:30 -0300

    adding information about react - Advanced JSX - 20 digits of pi in jsx

--- 2 - Advanced JSX/5 - 20-digits-of-pi-in-jsx.js
+++ 2 - Advanced JSX/5 - 20-digits-of-pi-in-jsx.js
@@ -10,12 +10,26 @@
 // Find Math. From there up through (20), the code will be treated as regular JavaScript again.
 // The curly braces themselves won’t be treated as JSX nor as JavaScript. They are markers that signal the beginning and end of a JavaScript injection into JSX, similar to the quotation marks that signal the boundaries of a string.
 
-
-
 // Select app.js.
-
 // Declare a new variable named math. Set math equal to a JSX <h1></h1> element.
-
 // Put the following text inside of the <h1>:
-
 // 2 + 3 = 2 + 3
+
+// At the bottom of the file, call ReactDOM.render().
+// For ReactDOM.render()‘s first argument, pass in math.
+// For ReactDOM.render()‘s second argument, pass in document.getElementById('app').
+
+// As you probably expected, the equation was displayed as a string.
+// Insert a pair of curly braces into the <h1></h1>, so that the browser displays 2 + 3 = 5.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const pi = (
+  <div>
+    <h1>PI, YALL!</h1>
+    <p>{Math.PI.toFixed(20)}</p>
+  </div>
+);
+
+ReactDOM.render(pi, document.getElementById("app"));
