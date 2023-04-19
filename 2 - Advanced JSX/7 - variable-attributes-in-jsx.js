18f0018f86a7ad10a702435c0469594706f7aa79
Fernanda Winter <fernanda.winter@hotmail.com>
2023-04-18 23:22:33 -0300

    adding information about react - Advanced JSX - variable attributes in jsx

--- 2 - Advanced JSX/7 - variable-attributes-in-jsx.js
+++ 2 - Advanced JSX/7 - variable-attributes-in-jsx.js
@@ -4,14 +4,14 @@
 // Here’s an example of how that might work:
 
 // // Use a variable to set the `height` and `width` attributes:
- 
+
 // const sideLength = "200px";
- 
+
 // const panda = (
-//   <img 
-//     src="images/panda.jpg" 
-//     alt="panda" 
-//     height={sideLength} 
+//   <img
+//     src="images/panda.jpg"
+//     alt="panda"
+//     height={sideLength}
 //     width={sideLength} />
 // );
 // Notice how in this example, the <img />‘s attributes each get their own line. This can make your code more readable if you have a lot of attributes on one element.
@@ -22,32 +22,39 @@
 //   panda: "http://bit.ly/1Tqltv5",
 //   owl: "http://bit.ly/1XGtkM3",
 //   owlCat: "http://bit.ly/1Upbczi"
-// }; 
- 
+// };
+
 // const panda = (
-//   <img 
-//     src={pics.panda} 
+//   <img
+//     src={pics.panda}
 //     alt="Lazy Panda" />
 // );
- 
+
 // const owl = (
-//   <img 
-//     src={pics.owl} 
+//   <img
+//     src={pics.owl}
 //     alt="Unimpressed Owl" />
 // );
- 
+
 // const owlCat = (
-//   <img 
-//     src={pics.owlCat} 
+//   <img
+//     src={pics.owlCat}
 //     alt="Ghastly Abomination" />
-// ); 
-
-
-
-
-
+// );
 
 // On line 9, declare a new variable named gooseImg. Set its value equal to a JSX <img /> element.
 // Give the <img /> an attribute with a name of src. Set the attribute’s value equal to the variable goose.
 
 // Use root.render() to render gooseImg.
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+const goose =
+  "https://content.codecademy.com/courses/React/react_photo-goose.jpg";
+
+// Declare new variable here:
+const gooseImg = <img src={goose} />;
+root.render(gooseImg);
