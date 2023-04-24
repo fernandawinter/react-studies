fa62aa73339e36240dae2a45603675f7d16322ee
Fernanda Winter <fernanda.winter@hotmail.com>
2023-04-23 22:37:52 -0300

    adding information about react - Advanced JSX - if statements that do work

---
+++ 2 - Advanced JSX/10 - if-statements-that-do-work.js
@@ -0,0 +1,10 @@
+// JSX Conditionals: If Statements That Do Work
+// How can you write a conditional if you can’t inject an if statement into JSX?
+
+// One option is to write an if statement and not inject it into JSX.
+
+// Look at if.js. Follow the if statement, all the way from line 8 down to line 20.
+
+// if.js works because the words if and else are not injected in between JSX tags. The if statement is on the outside, and no JavaScript injection is necessary.
+
+// This is a common way to express conditionals in JSX.
