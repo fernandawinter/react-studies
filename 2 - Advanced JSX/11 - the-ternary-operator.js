a2d456e83ca9fbfbe63936f9d924ec3b13e66c54
Fernanda Winter <fernanda.winter@hotmail.com>
2023-04-26 15:38:01 -0300

    adding information about react - Advanced JSX - the ternary operator

--- 2 - Advanced JSX/11 - the-ternary-operator.js
+++ 2 - Advanced JSX/11 - the-ternary-operator.js
@@ -13,3 +13,29 @@
 //     </h1>
 // );
 // In the above example, if age is greater than or equal to drinkingAge, then headline will equal < h1 > Buy Drink</h1 >.Otherwise, headline will equal < h1 > Do Teen Stuff</h1 >.
+
+
+
+
+
+// Take a look at app.js.
+// On line 16, replace x, y, and z so that the expression returns 'kitty' if coinToss() is 'heads' and 'doggy' if coinToss() is 'tails'.
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+function coinToss() {
+    // Randomly return either 'heads' or 'tails'.
+    return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+const pics = {
+    kitty: 'https://content.codecademy.com/courses/React/react_photo-kitty.jpg',
+    doggy: 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg'
+};
+
+const img = <img src={pics[coinToss() === 'heads' ? 'kitty' : 'doggy']} />;
+
+root.render(img);
