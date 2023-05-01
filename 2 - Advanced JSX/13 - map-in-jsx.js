38ab3a4e3e422ee15e930de53b52ca961d2c3624
Fernanda Winter <fernanda.winter@hotmail.com>
2023-04-30 22:02:14 -0300

    adding information about react - Advanced JSX - map in jsx

---
+++ 2 - Advanced JSX/13 - map-in-jsx.js
@@ -0,0 +1,29 @@
+// .map in JSX
+// The.map() array method comes up often in React.It’s good to get in the habit of using it alongside JSX.
+
+// If you want to create a list of JSX elements, then using.map() is often the most efficient way.It can look odd at first:
+
+// const strings = ['Home', 'Shop', 'About Me'];
+
+// const listItems = strings.map(string => <li>{string}</li>);
+
+// <ul>{listItems}</ul>
+// In the above example, we start out with an array of strings.We call.map() on this array of strings, and the.map() call returns a new array of < li > s.
+
+// On the last line of the example, note that { listItems } will evaluate to an array, because it’s the returned value of.map()! JSX < li > s don’t have to be in an array like this, but they can be.
+
+// // This is fine in JSX, not in an explicit array:
+// < ul >
+//   <li>item 1</li>
+//   <li>item 2</li>
+//   <li>item 3</li>
+// </ul >
+
+// // This is also fine!
+// const liArray = [
+//     <li>item 1</li>,
+//     <li>item 2</li>,
+//     <li>item 3</li>
+// ];
+
+// <ul>{liArray}</ul>
