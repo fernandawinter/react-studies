dc4ec864df1202a4128132b0c7ba41c6fa456310
Fernanda Winter <fernanda.winter@hotmail.com>
2023-05-16 17:57:25 -0300

    adding information about react - React Components - import react dom

---
+++ 3 - React Components/3 - import-react-dom.js
@@ -0,0 +1,9 @@
+// Import ReactDOM
+// Another import we need in addition to React is ReactDOM:
+
+// import ReactDOM from 'react-dom/client';
+// The methods imported from 'react-dom' interact with the DOM.
+
+// The methods imported from 'react' do not deal with the DOM at all. They don’t engage directly with anything that isn’t part of React.
+
+// To clarify: the DOM is used in React applications, but it isn’t part of React. After all, the DOM is also used in countless non-React applications. Methods imported from 'react' are only for pure React purposes, such as creating components or writing JSX elements.
