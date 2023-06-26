9465c15eb2201b2de71d7191e5e5f4245c9af84c
Fernanda Winter <fernanda.winter@hotmail.com>
2023-06-26 19:54:49 -0300

    adding information about react - Components and advanced jsx - use multiline jsx in a component

--- 4 - Components and advanced jsx/1 - use-multiline-jsx-in-a-component.js
+++ 4 - Components and advanced jsx/1 - use-multiline-jsx-in-a-component.js
@@ -47,3 +47,60 @@
 
 // Use QuoteMaker.js as a guide! Remember to import React at the top of the file, and remember to export your component at the bottom of the file.
 // If you don’t like this quote, feel free to use a different one.
+
+
+//QuoteMaker.js
+import React from 'react';
+
+function QuoteMaker() {
+    return (
+        <blockquote>
+            <p>
+                The world is full of objects, more or less interesting; I do not wish to add any more.
+            </p>
+            <cite>
+                <a target="_blank"
+                    href="https://en.wikipedia.org/wiki/Douglas_Huebler">
+                    Douglas Huebler
+                </a>
+            </cite>
+        </blockquote>
+    );
+};
+
+export default QuoteMaker;
+
+
+
+//MyQuote.js
+import React from 'react';
+
+function MyQuote() {
+    return (
+        <blockquote>
+            <p>
+                What is important now is to recover our senses.
+            </p>
+            <cite>
+                <a target="_blank" href="https://en.wikipedia.org/wiki/Susan_Sontag">
+                    Susan Sontag
+                </a>
+            </cite>
+        </blockquote>
+    );
+};
+
+export default MyQuote;
+
+
+//App.js
+import React from 'react';
+import MyQuote from './MyQuote'
+
+function App() {
+    return (
+        <MyQuote />
+    );
+};
+
+export default App;
