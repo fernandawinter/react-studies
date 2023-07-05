52bb5164063916c2bec4ecc7429ae833bbfffa22
Fernanda Winter <fernanda.winter@hotmail.com>
2023-07-04 23:22:08 -0300

    adding information about react - Components and advanced jsx - use a variable attribute in a component

---
+++ 4 - Components and advanced jsx/2 - use-a-variable-attribute-in-a-component.js
@@ -0,0 +1,13 @@
+// Use a Variable Attribute in a Component
+// Take a look at this JavaScript object named redPanda:
+
+// const redPanda = {
+//   src: 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Endangered_Red_Panda.jpg',
+//   alt: 'Red Panda',
+//   width: '200px
+// };
+// How could you render a React component with an image of redPanda and its properties?
+
+// Select RedPanda.js to see one way to do this.
+
+// Note all the curly brace JavaScript injections inside the return statement. You can, and often will, inject JavaScript into JSX inside the return statement.
