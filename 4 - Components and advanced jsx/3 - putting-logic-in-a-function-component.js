7562ab82e0589c0ee7511cf4d58b300efc4fa5e2
Fernanda Winter <fernanda.winter@hotmail.com>
2023-08-06 23:13:00 -0300

    adding information about react - Components and advanced jsx - putting logic in a function component

--- 4 - Components and advanced jsx/3 - putting-logic-in-a-function-component.js
+++ 4 - Components and advanced jsx/3 - putting-logic-in-a-function-component.js
@@ -45,3 +45,34 @@
 // Finally, let’s meet your new friend by adding a picture of them.
 // Still inside of the <div></div> tags, make a new line after the <h1></h1> tags.
 // On the new line, add an <img /> element. Give the <img /> tag an attribute of src with the value of {friend.src}.
+
+//Friend.js
+import React from 'react';
+
+const friends = [
+    {
+        title: "Yummmmmmm",
+        src: "https://content.codecademy.com/courses/React/react_photo-monkeyweirdo.jpg"
+    },
+    {
+        title: "Hey Guys! Wait Up!",
+        src: "https://content.codecademy.com/courses/React/react_photo-earnestfrog.jpg"
+    },
+    {
+        title: "Yikes",
+        src: "https://content.codecademy.com/courses/React/react_photo-alpaca.jpg"
+    }
+];
+
+// New function component starts here:
+function Friend() {
+    let friend = friends[0];
+    return (
+        <div>
+            <h1>{friend.title}</h1>
+            <img src={friend.src} />
+        </div>
+    );
+}
+
+export default Friend;
