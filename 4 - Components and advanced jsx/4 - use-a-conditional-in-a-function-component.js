aeba15be020eefa2ff62b52401e3dc8e91867e50
Fernanda Winter <fernanda.winter@hotmail.com>
2023-08-15 22:39:56 -0300

    adding information about react - Components and advanced jsx - use a conditional in a function component

---
+++ 4 - Components and advanced jsx/4 - use-a-conditional-in-a-function-component.js
@@ -0,0 +1,6 @@
+// Use a Conditional in a Function Component
+// How might you use a conditional statement inside of a function component?
+
+// Select TodaysPlan.js to see one way of doing it.
+
+// Notice that the if statement is located inside of the function component, but before the return statement.
